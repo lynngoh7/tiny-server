cdc80f9e27e1cadac92ac21c877f4800bc3c7a15
lynngoh7 <gohilynn@gmail.com>
2026-07-14 18:50:31 +0800

    Stage 1: root and health endpoints

--- server.js
+++ server.js
@@ -2,8 +2,26 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+let tasks = [ 
+  { id: 1, title: "Do homework", completed: false },
+  { id: 2, title: "Walk the dog", completed: true },
+  { id: 3, title: "Read a book", completed: false }
+];
+
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello, world!' });
+  res.json({
+    name: "Task API",
+    version : "1.0",
+    endpoints:  ["/tasks"]
+   });
+});
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK' });
 });
 
 app.get('/time', (req, res) => {
